fb6fadb09dfdde02093177f9736a06b24a05e007
Ayakashi <yoshidarekadzava1768@gmail.com>
2026-03-04 22:10:55 +0900

    finish

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 const button = document.getElementById("toggleBrands");
 const block = document.getElementById("brandsList");
 button.addEventListener("click", function () {
-  if (block.style.height === "300px") {
-    block.style.display = "auto";
+  if (block.style.height === "160px") {
+    block.style.height = "auto";
     button.innerHTML =
       ' <img class="expand-icon icon-rotate" src="icon/icon.svg" alt="" /> <span class="expand-text ">Скрыть</span>';
   } else {
-    block.style.height = "300px";
+    block.style.height = "160px";
     button.innerHTML =
       ' <img class="expand-icon" src="icon/icon.svg" alt="" /> <span class="expand-text">Показать все</span>';
   }
